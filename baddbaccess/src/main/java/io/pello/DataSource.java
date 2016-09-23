ca13b9522b79c706c99fd2177d1df46d1151386f
pxai <pello_altadill@cuatrovientos.org>
2016-09-23 12:14:53 +0200

    Added DataSource

--- baddbaccess/src/main/java/io/pello/DataSource.java
+++ baddbaccess/src/main/java/io/pello/DataSource.java
@@ -1,5 +1,45 @@
 package io.pello;
 
-public class DataSource {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+
+/**
+ * 
+ * @author pello altadill
+ *
+ */
+public class DataSource {
+	
+	private Connection connection;
+	
+	public DataSource () {
+		init();
+	}
+	
+	private void init () {
+		try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			    connection =
+			      DriverManager.getConnection("jdbc:mysql://localhost/pruebas","root","");
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * @return the connection
+	 */
+	public Connection getConnection() {
+		return connection;
+	}
+
+	/**
+	 * @param connection the connection to set
+	 */
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 }
+
